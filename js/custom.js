a1d35e127a0c4188d37d1e3cee45a87113a23098
David Chen <chendavid96@gmail.com>
2016-08-20 05:10:11 -0400

    js script for user nav and euler page updates

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$(function(){
+	// load footer content
+	$("#footerContent").load("../primary-footer.html");
+
+	// load page links
+	$("#homePageLinksContent").load("../primary-page-links.html", function(){
+
+		// lets user know what page they are currently on
+		var pathname = window.location.pathname;
+		$(".home-page-link").find('a').each(function () {
+			var linktext = $(this).text();
+			if(pathname.indexOf(linktext) >= 0){
+				$(this).before("| ")
+			}
+		});
+
+
+	});
+
+});
