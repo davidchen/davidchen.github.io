0fd1d6707768fc58caee794e73ec4de4d03bcf7c
David Chen <chendavid96@gmail.com>
2016-08-22 00:47:02 -0400

    added some euler questions

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,6 @@ $(function(){
 
 	// load page links
 	$("#homePageLinksContent").load("../primary-page-links.html", function(){
-
 		// lets user know what page they are currently on
 		var pathname = window.location.pathname;
 		$(".home-page-link").find('a').each(function () {
@@ -16,8 +15,18 @@ $(function(){
 				$(this).before("| ")
 			}
 		});
-
-
 	});
+
+	// loops through each element with the class ".euler-problem-title"
+	$(".euler-problem-title").each(function (i) {
+		var idCount = i+1;
+		// assigns appropriate id tags for each euler problem (ids are given in order of the problems)
+		$(this).attr("id", "euler"+idCount);
+		// creates anchor links for euler problems up to however many are on page
+		$(".euler-anchor-links").append("<a href='#euler" + idCount + "'><div class='euler-anchor-link'>" + idCount + "</div></a>");
+	});
+
+	
+
 
 });
